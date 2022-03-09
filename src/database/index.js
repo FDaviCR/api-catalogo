4f7503e5f04424ce392affc614d54ec8e5167bfa
Davi <davicamiloribeiro@gmail.com>
2022-03-09 19:22:52 -0300

    Base do projeto

---
+++ src/database/index.js
@@ -0,0 +1,24 @@
+const Sequelize = require('sequelize');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const connection = new Sequelize(
+    process.env.MYSQL_DB,
+    process.env.MYSQL_USER,
+    process.env.MYSQL_PASSWORD,
+    {
+        dialect: 'mysql',
+        port: parseInt(process.env.MYSQL_PORT)
+    }    
+);
+/*
+connection.authenticate()
+    .then(()=>{
+        console.log("Conexão feita com o banco de dados!")
+    })
+    .catch((msgErro)=>{
+        console.log(msgErro);
+    })
+*/
+module.exports = connection;
---
+++ src/server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const session = require("express-session");
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+
+const dotenv = require('dotenv');
+
+const JWTSecret = "YouShallNotPass";
+
+dotenv.config();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+function auth(req, res, next){
+    const authToken = req.headers['authorization'];
+
+    if(authToken != undefined){
+        const bearer = authToken.split(' ');
+        var token = bearer[1];
+        //console.log(token);
+
+        jwt.verify(token, JWTSecret,(err, data) => {
+            if(err){
+                res.status(401);
+                res.json({err:"Token inválido!"});
+            }else{
+                req.token = token;
+                req.loggedUser = {id: data.id, login: data.login};
+                next();
+            }
+        });
+    }else{
+        res.status(401);
+        res.json({err:"Token inválido!"});
+    }
+}
+
+app.listen(process.env.PORT, () => {
+    console.log("O servidor está rodando!")
+});
